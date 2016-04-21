29a2ba90d23550cc656dbed66170a83c13edb220
Dylan Wylie <dylanwylie@gmail.com>
2016-04-21 11:50:34 -0400

    handler

--- lib/handler.ex
+++ lib/handler.ex
@@ -1,9 +1,13 @@
 defmodule WebService.Handler do
 
   def init({:tcp, :http}, req, opts) do
-    headers = [{"content-type", "text.plain" }]
+
+
     {param, req} = :cowboy_req.binding(:n, req)
+    start_time = :os.system_time(:milli_seconds)
     body = to_string(Fibonacci.fib(String.to_integer(param)))
+    finish_time = :os.system_time(:milli_seconds)
+    headers = [{"content-type", "text.plain" }, {"x_fib_time", to_string(finish_time-start_time)}]
     {:ok, resp} = :cowboy_req.reply(200, headers, body, req)
     {:ok, resp, opts} 
   end
@@ -14,9 +18,5 @@ defmodule WebService.Handler do
 
   def terminate(_reason, _req, _state) do
     :ok
-  end
-
-  def fib_wrapper(n) do
-
   end
 end
