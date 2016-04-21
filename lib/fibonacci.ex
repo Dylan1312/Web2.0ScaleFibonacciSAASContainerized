7ae5cdecbe060924992d72e975b7fb306ba9ebd3
Dylan Wylie <dylanwylie@gmail.com>
2016-04-21 12:32:34 -0400

    memo it!

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -1,12 +1,19 @@
 defmodule Fibonacci do
-  def fib(a) do
-    cond do
-      a <= 0 or a >= 100 ->
-        raise ArgumentError, message: "argument must be greater than 0 and less than 100"
-      a > 2 ->
-        fib(a-1) + fib(a-2)
-      true ->
-        1
+  def fib(a, memo \\ %{}) do
+    
+    if Map.has_key?(memo, a) do
+      Map.get(memo, a)
+    else
+      cond do
+        a <= 0 or a >= 100 ->
+          raise ArgumentError, message: "argument must be greater than 0 and less than 100"
+        a > 2 ->
+          f = fib(a-1, memo) + fib(a-2, memo)
+          Map.put(memo, a, f)
+          f
+        true ->
+          1
+      end
     end
   end
 end
