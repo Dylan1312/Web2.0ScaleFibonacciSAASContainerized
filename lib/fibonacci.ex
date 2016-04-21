4cae4a8c9fdeaf8ae92a74d5d65b6e548414fcd4
Dylan Wylie <dylanwylie@gmail.com>
2016-04-21 12:48:16 -0400

    memo 2

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -8,9 +8,9 @@ defmodule Fibonacci do
         a <= 0 or a >= 100 ->
           raise ArgumentError, message: "argument must be greater than 0 and less than 100"
         a > 2 ->
-          f = fib(a-1, memo) + fib(a-2, memo)
-          Map.put(memo, a, f)
-          f
+          f1 = fib(a-2, memo)
+          Map.put(memo, a-2, f1)
+          f1 + fib(a-1, memo)
         true ->
           1
       end
